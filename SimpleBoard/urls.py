0174dea7e39df2939fe2a587c29d0640820776cf
strviola <the-brightest-moonlight.8107@mbp.nifty.com>
2013-06-18 10:44:52 +0900

    First commit

---
+++ SimpleBoard/urls.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+from django.conf.urls import patterns, include, url
+import Article.views as article
+from django.contrib import admin
+
+# Uncomment the next two lines to enable the admin:
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^test/$', article.test),
+    url(r'^articles/(?P<article_id>\d+)/$', article.detail),
+    url(r'^form/$', article.form),
+    url(r'^form/post/$', article.post),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', article.headline),
+)
