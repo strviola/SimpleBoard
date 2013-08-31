d4e027c15e695db28d9ebbdbe145078c59ad3de3
Dragon <suzuki.r.ak@m.titech.ac.jp>
2013-08-31 18:52:48 +0900

    Add a file for statistics analyze

---
+++ SimpleBoard/middleware/security.py
@@ -0,0 +1,12 @@
+'''
+Created on 2013/08/31
+
+@author: SuzukiRyota
+'''
+
+
+class StatisticsAnalyzerMiddleware(object):
+    
+    def process_view(self, request, callback, callback_args, callback_kwargs):
+        pass
+    
